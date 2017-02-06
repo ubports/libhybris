da9974ff38ed478b32c442f94cbd841a4b1f583b
Kevin DuBois <kevin.dubois@canonical.com>
2017-02-06 15:38:13 -0500

    put the appropriate copyright header in

--- hybris/egl/platforms/mir/eglplatform_mir.cpp
+++ hybris/egl/platforms/mir/eglplatform_mir.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2017 Canonical Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
 #define ANDROID
 #include <EGL/eglplatform.h>
 #include <mir_toolkit/mir_extension_core.h>
@@ -119,11 +137,6 @@ struct MirDisplay : MirBaseDisplay
         connection(con),
         ext(ext)
     {
-    }
-
-    EGLNativeDisplayType to_native_display_type(MirConnection* connection)
-    {
-        return ext->to_display(connection);
     }
 
     struct _EGLNativeWindowType* create_window(
